bf50465600be642bb1defed6e7bd18d7caefa68c
Ankur1903 <ankurzalavadiya11@gmail.com>
2020-09-12 21:59:20 +0530

    Updates

---
+++ precache-manifest.2dd42e3d02a3c813b36c43d5bb088e29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9009464a73079add2bdc0b18f206b9e6",
+    "url": "/imovie/index.html"
+  },
+  {
+    "revision": "e3b49aa428c2e333bd50",
+    "url": "/imovie/static/css/main.ec7aa0cb.chunk.css"
+  },
+  {
+    "revision": "9c10111163826d9344f1",
+    "url": "/imovie/static/js/2.cc6b9938.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/imovie/static/js/2.cc6b9938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3b49aa428c2e333bd50",
+    "url": "/imovie/static/js/main.0e317799.chunk.js"
+  },
+  {
+    "revision": "1634552221797108285d",
+    "url": "/imovie/static/js/runtime-main.672413b9.js"
+  }
+]);
